6965398ff2e6900d5c974f43a82ea6579714e087
Satish5688 <satishmungusmare5688@gmail.com>
2024-09-14 09:45:41 +0530

    Divide Pole infomation in categories

--- pages/pole/detail.js
+++ pages/pole/detail.js
@@ -20,8 +20,43 @@ export default function Pole() {
   const params = useSearchParams();
   const row = poles?.find((row) => row._rowNumber == params.get("pole"));
   const router = useRouter();
-  console.log(row);
   const district = useSearchParams()?.get("district");
+  const categories = {
+    "Location Information": [
+      "district",
+      "ULB Name",
+      "Ward No",
+      "Pole Land Mark/Location",
+      "Lattitude",
+      "Longitude",
+    ],
+    "Pole Structure": [
+      "Pole type",
+      "Pole arrangement",
+      "Pole Height",
+      "Old / New",
+    ],
+    "Power & Electrical Details": [
+      "CCMS/Timer",
+      "CCMS Rating (KW)",
+      "Rating (W)",
+      "Nos.",
+    ],
+    "Cable Specifications": [
+      "Cable type",
+      "Cable Rating (Sq.mm)",
+      "Cable length New Installed (m)",
+    ],
+    "Arm Details": ["Arm Type", "Arm Length"],
+    "Accessories & Fixtures": [
+      "Coil",
+      "GI Pipe",
+      "Suspension Clamp",
+      "Dead End Clamp",
+      "Eye-hook",
+    ],
+    "Installation Information": ["Date of installation", "Remarks"],
+  };
 
   const handleDelete = (row) => {
     setLoading(true);
@@ -29,7 +64,12 @@ export default function Pole() {
       console.log(row, "deleted....");
       loadPoles();
       setLoading(false);
-      router.push("/switch?area=" + row.get("Area Code") + "&switch=" + row.get("Switch No."));
+      router.push(
+        "/switch?area=" +
+          row.get("Area Code") +
+          "&switch=" +
+          row.get("Switch No.")
+      );
     });
   };
 
@@ -51,14 +91,17 @@ export default function Pole() {
       </div>
       <div className="mx-5 lg:mx-[27%] mt-3">
         <h4 className="text-gray-600 text-sm">
-        <Link className="hover:text-blue-500" href={"/"}>
+          <Link className="hover:text-blue-500" href={"/"}>
             DISTRICT
           </Link>
           <FontAwesomeIcon
             className="text-[8px] text-gray-700 ml-2 mr-2 -mt-4"
             icon={faChevronRight}
           />
-          <Link className="hover:text-blue-500" href={`/ulb?district=${district}`}>
+          <Link
+            className="hover:text-blue-500"
+            href={`/ulb?district=${district}`}
+          >
             {district}
           </Link>
           <FontAwesomeIcon
@@ -90,20 +133,36 @@ export default function Pole() {
             Delete
           </button>
         </div>
-        <div className="py-5 grid grid-cols-1 gap-3 lg:grid-cols-2">
-          
+        
+        <div className="py-5">
           {row &&
-            Object.keys(row.toObject())?.map(
-              (col) =>
-                row.get(col) && (
-                  <div className="flex justify-between lg:flex-col flex-row lg:border-none border-b py-3 border-gray-200">
-                    <h4 className="text-gray-600 font-medium">{col}</h4>
-                    <h4 className="text-gray-800 font-medium">
-                      {row.get(col)}
-                    </h4>
-                  </div>
-                )
-            )}
+            Object.keys(categories).map((category, index) => (
+              <div key={category} className="mb-5">
+                <h3 className="text-lg font-semibold text-gray-600 mb-3 ">
+                  {category}
+                </h3>{" "}
+                <div className="grid grid-cols-1 gap-3 lg:grid-cols-2">
+                  {categories[category].map(
+                    (col) =>
+                      row.get(col) && (
+                        <div
+                          key={col}
+                          className="flex justify-between lg:flex-col flex-row border-none border-b py-3 border-gray-200"
+                        >
+                          <h4 className="text-gray-600 font-medium">{col}</h4>
+                          <h4 className="text-gray-800 font-medium">
+                            {row.get(col)}
+                          </h4>
+                        </div>
+                      )
+                  )}
+                </div>
+                {index < Object.keys(categories).length - 1 && (
+                  <hr className="my-4 border-gray-300" />
+                )}{" "}
+                {/* Add line after each category except the last one */}
+              </div>
+            ))}
         </div>
       </div>
     </>
